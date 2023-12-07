57989c7783d77180aeeed8618b3168d0463dc472
Abod08 <ayindeabodunrin@gmail.com>
2023-12-07 17:26:33 +0100

    Composition

---
+++ Name.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace New_folder
+{
+    public class Name
+    {
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+        public class School
+        {
+            public string Name { get; set; }
+            
+        }
+    }
+
+    // public class
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using New_folder;
+
+Console.WriteLine("Hello, World!");
+Name name = new();
